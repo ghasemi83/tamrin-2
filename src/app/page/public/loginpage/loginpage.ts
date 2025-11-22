438dea865ee4fcadd28ce6516725e2661b24f5ac
ghasemi83 <mahshidg752@gmail.com>
2025-11-22 07:44:44 -0800

    i ran into a problem

--- src/app/page/public/loginpage/loginpage.ts
+++ src/app/page/public/loginpage/loginpage.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { routes } from '../../../app.routes';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-loginpage',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './loginpage.html',
   styleUrl: './loginpage.scss',
 })
 export class Loginpage {
-
-}
+  username:string='';
+  password:string='';
+  remember:boolean=false;
+  router=inject(Router);
+  massage:string=''
+  check(){
+   if (this.username=='admin' && this.password=='admin ') {
+    this.router .navigateByUrl('private');
+   } else{
+    this.massage='نام کاربری ضحیح نیست ';
+   }
+}}
